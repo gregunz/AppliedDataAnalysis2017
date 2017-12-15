5c40945644823f7538b106604d0d213fe8878a84
gr3g <gregoire.clement@epfl.ch>
2017-12-15 16:13:09 +0100

    remove notebooks

--- project/src/load_data.py
+++ project/src/load_data.py
@@ -1,25 +1,26 @@
-import pandas as pd
 import datetime
-import requests
 import os
 
-from high_level_fetch import get_cleaned_month
-from fetch_gdelt_data import fetch_df
-from clean_data import default_columns, cleaned_columns, country_to_cca3, dict_countries_to_cca3
+import pandas as pd
 from tqdm import tqdm
 
+from clean_data import cleaned_columns, country_to_cca3, dict_countries_to_cca3
+from high_level_fetch import get_cleaned_month
+
 final_column_names = ['Day', 'EventCode', 'Source_CountryCode', 'Target_CountryCode',
-                    'Target_Lat', 'Target_Long', 'Target_GeoType', 'IsRootEvent',
-                    'QuadClass', 'GoldsteinScale', 'AvgTone', 'NumMentions',
-                    'NumSources', 'NumArticles']
+                      'Target_Lat', 'Target_Long', 'Target_GeoType', 'IsRootEvent',
+                      'QuadClass', 'GoldsteinScale', 'AvgTone', 'NumMentions',
+                      'NumSources', 'NumArticles']
+
 
 def load_data():
     """This function loads all the data cleaned from 03/01/201 to 12/12/2017.
         In most cases, only this function should be called
     """
     if not os.path.isfile('../data/final_data.csv'):
-        #fetching
-        print("If you see this and you are not expecting to download and clean all the data, please verify you have the file final_data.csv in the folder data")
+        # fetching
+        print(
+            "If you see this and you are not expecting to download and clean all the data, please verify you have the file final_data.csv in the folder data")
 
         print("Fetching and cleaning GDELT 2.0 Translingual data...")
         start_date = datetime.datetime(2015, 3, 1)
@@ -31,21 +32,20 @@ def load_data():
 
         for i in tqdm(range(n_months)):
             index = start_date.month - 1 + i
-            
+
             from_month = index % 12 + 1
             from_year = start_date.year + index // 12
-            
+
             df = df.append(get_cleaned_month(from_month, from_year))
 
         print("End fetching")
         print("Finalising cleaning...")
-        #Country cleaning
-
+        # Country cleaning
 
         df.columns = ['EventCode', 'Target_CountryCode_2', 'Target_Lat', 'Target_Long',
-                    'IsRootEvent', 'QuadClass', 'GoldsteinScale', 'AvgTone',
-                    'NumMentions', 'NumSources', 'NumArticles', 'Target_GeoType',
-                    'Day', 'Target_CountryCode', 'Source_CountryName', 'Target_CountryName']
+                      'IsRootEvent', 'QuadClass', 'GoldsteinScale', 'AvgTone',
+                      'NumMentions', 'NumSources', 'NumArticles', 'Target_GeoType',
+                      'Day', 'Target_CountryCode', 'Source_CountryName', 'Target_CountryName']
 
         df['Source_CountryCode'] = df['Source_CountryName'].apply(lambda x: country_to_cca3(x, dict_countries_to_cca3))
 
@@ -53,7 +53,7 @@ def load_data():
 
         df = df[discard_mask]
         df = df[final_column_names]
-        
+
         print("End cleaning")
         print("Writing...")
 
